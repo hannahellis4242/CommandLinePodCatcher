27621670cda7980740b62e676b7ee1a0f954412d
hannahellis4242 <hannahellis4242@gmail.com>
2023-09-30 22:20:10 +0100

    It's messy but it works

--- src/handlers/handlePull.ts
+++ src/handlers/handlePull.ts
@@ -5,6 +5,7 @@ import exists from "./utils/exitsts";
 import readConfigFile from "./utils/readConfigFile";
 import { mkdir, writeFile } from "fs/promises";
 import Channel from "../model/podcasts/Channel";
+import download from "./utils/download";
 
 const getChannels = (config: Config): Channel[] =>
   config.feeds
@@ -12,12 +13,13 @@ const getChannels = (config: Config): Channel[] =>
     .map((feed) => feed.channel)
     .map((channel) => channel!);
 
-const download = ({ url, filePath }: Episode) =>
-  axios(url).then(({ data }) => writeFile(filePath, data));
-
 const handlePull = async (path: string) => {
   try {
     const config = await readConfigFile(path);
+    const podcastsDirExists = await exists(config.path);
+    if (!podcastsDirExists) {
+      await mkdir(config.path);
+    }
     const channels = getChannels(config);
     for (const channel of channels) {
       const dirExists = await exists(channel.path);
