c0f192c7c29104e685c56a0ff61a87106e219dc6
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-01 11:07:22 +0100

    try even harder to get episode

--- src/handlers/utils/download.ts
+++ src/handlers/utils/download.ts
@@ -1,16 +1,34 @@
 import { writeFile } from "fs/promises";
 import Episode from "../../model/podcasts/Episode";
-import { get } from "superagent";
+import { get, Response } from "superagent";
 import { promisify } from "util";
 
 const getPromise = promisify(get);
 
+const getWithRedirect = async (url: string): Promise<Response> => {
+  const res = await getPromise(url);
+  if (res.statusCode === 200) {
+    return res;
+  }
+  const { headers } = res;
+  const { location } = headers;
+  if (location) {
+    return await getWithRedirect(location);
+  }
+  return Promise.reject(new Error("no more redirects found"));
+};
+
 const download = async (episode: Episode) => {
   try {
-    const res = await getPromise(episode.url);
-    await writeFile(episode.filePath, res.body);
+    const res = await getWithRedirect(episode.url);
+    if (res.status === 200) {
+      await writeFile(episode.filePath, res.body);
+      return;
+    }
+    return Promise.reject(new Error("could not get episode"));
   } catch (err) {
     console.error(err);
+    return Promise.reject(err);
   }
 };
 
