fdeb88a5006171093303639975e88a050b269af9
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-01 20:01:33 +0100

    Fixing download Seems to work ok now

--- src/handlers/utils/download.ts
+++ src/handlers/utils/download.ts
@@ -1,35 +1,10 @@
-import { writeFile } from "fs/promises";
 import Episode from "../../model/podcasts/Episode";
-import { get, Response } from "superagent";
-import { promisify } from "util";
+import downloadHttps from "./downloadHttps";
+import downloadHttp from "./downloadHttp";
 
-const getPromise = promisify(get);
-
-const getWithRedirect = async (url: string): Promise<Response> => {
-  const res = await getPromise(url);
-  if (res.statusCode === 200) {
-    return res;
-  }
-  const { headers } = res;
-  const { location } = headers;
-  if (location) {
-    return await getWithRedirect(location);
-  }
-  return Promise.reject(new Error("no more redirects found"));
-};
-
-const download = async (episode: Episode) => {
-  try {
-    const res = await getWithRedirect(episode.url);
-    if (res.status === 200) {
-      await writeFile(episode.filePath, res.body);
-      return;
-    }
-    return Promise.reject(new Error("could not get episode"));
-  } catch (err) {
-    console.error(err);
-    return Promise.reject(err);
-  }
-};
+const download = ({ url, filePath }: Episode): Promise<void> =>
+  url.includes("https")
+    ? downloadHttps(url, filePath)
+    : downloadHttp(url, filePath);
 
 export default download;
