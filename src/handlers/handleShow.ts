38d7f986d015d2c775b2c2d85687aa3aaf828dcb
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-02 07:58:20 +0100

    wip

--- src/handlers/handleShow.ts
+++ src/handlers/handleShow.ts
@@ -1,11 +1,36 @@
+import Channel from "../model/podcasts/Channel";
+import Episode from "../model/podcasts/Episode";
+import Feed from "../model/podcasts/Feed";
 import readConfigFile from "./utils/readConfigFile";
+
+const showEpisode = ({ title, subscribe }: Episode): string =>
+  `[${subscribe ? "*" : " "}] ${title}`;
+
+const showChannel = ({ title, episodes }: Channel): string => {
+  const episodesStr = episodes
+    .map(showEpisode)
+    .map((s) => `\t${s}`)
+    .join("\n");
+  return `${title}\n${episodesStr}`;
+};
+
+const showFeed = ({ id, url, channel }: Feed): string => {
+  return `${id}\n${url}\n${
+    channel ? showChannel(channel) : "no channel information available"
+  }`;
+};
 
 const handleShow = async (
   path: string,
-  id: string | undefined,
-  title: string | undefined
+  targetId: string | undefined,
+  targetTitle: string | undefined
 ) => {
   const config = await readConfigFile(path);
+  const feeds = config.feeds
+    .filter(({ id }) => id === targetId)
+    .filter(({ channel }) => (channel ? channel.title === targetTitle : true));
+  const output = feeds.map(showFeed).join("\n");
+  console.log(output);
 };
 
 export default handleShow;
